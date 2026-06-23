c64d77c7789d24a916f01093424e6cbfabbf81d1
Felipe Hermosilla <fahermosilla@uc.cl>
2026-06-23 15:47:38 -0400

    Entrega 3

---
+++ config.js
@@ -0,0 +1,13 @@
+Protobject.setProduction(true);
+
+Protobject.initialize([
+  {
+    name: "Visualizacion",
+    page: "index.html",
+    main: true
+  },
+  {
+    name: "Camara",
+    page: "aruco.html"
+  }
+]);
